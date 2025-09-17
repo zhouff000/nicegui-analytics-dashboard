4f9494c8f24f0928b1fde03003ba865390aa14c4
Arcolaus <arco.work@outlook.com>
2025-09-17 14:42:48 +0800

    construct charactor comprehension prompt

---
+++ src/backend/character_comprehension.py
@@ -0,0 +1,20 @@
+from openai import OpenAI
+import os
+import dotenv
+import tomllib
+
+dotenv.load_dotenv("./.env")
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+
+def prompt_template(character, scenario):
+    with open("src/backend/config/prompt.toml", "rb") as f:
+        config = tomllib.load(f)["character_comprehension"]
+
+    return config[scenario].format(character=character)
+
+
+
+if __name__ == "__main__":
+    print(prompt_template("林", "pronunciation"))
+    ...
