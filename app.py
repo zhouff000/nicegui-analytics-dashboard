f8e43ea4db49f3e29f08df16849bf7cbdb66c4c6
Arcolaus <arco.work@outlook.com>
2025-09-17 15:36:49 +0800

    optimize header html code

--- app.py
+++ app.py
@@ -5,18 +5,46 @@ with open("src/web/static/html/header.html", "r") as header_file:
 
 
 history = [
-    {"role": "assistant", "content": "I am happy to provide you that report and plot.","path":"documents/report.pdf"},
-    {"role": "user", "content": "Please provide a report and plot for the data I am uploading."},
+    {
+        "role": "assistant",
+        "content": "I am happy to provide you that report and plot.",
+        "path": "documents/report.pdf",
+    },
+    {
+        "role": "user",
+        "content": "Please provide a report and plot for the data I am uploading.",
+    },
 ]
 
 
 with gr.Blocks(
     theme=gr.themes.Soft(),
 ) as demo:
-    gr.HTML(header)
+    gr.HTML(header,container=True)
+    with gr.Tab("发音"):
+        gr.Chatbot(
+            [
+                {
+                    "role": "assistant",
+                    "content": "I am happy to provide you that report and plot.",
+                    "path": "documents/report.pdf",
+                },
+            ],
+            type="messages",
+        )
+    with gr.Tab("小练习"):
+        gr.Chatbot(
+            [
+                {
+                    "role": "user",
+                    "content": "Please provide a report and plot for the data I am uploading.",
+                },
+            ],
+            type="messages",
+        )
 
-    gr.Chatbot(history, type="messages")
-
+    gr.Textbox(placeholder="Type your message here...", label="Your Message")
+    gr.Button("Send")
     with gr.Sidebar(label="功能切换", position="left"):
         with gr.Column():
             gr.Markdown("## 大模型在线体验平台")
