8f120c614d00d1a5569da43f4c32dd175334ed33
Arcolaus <arco.work@outlook.com>
2025-09-24 02:47:20 +0800

    clear Gradio Multipage App

--- src/web/pages/__init__.py
+++
@@ -1,5 +0,0 @@
-from .dashboard.dashboard import dashboard
-from .character_resolution.character_resolution import character_resolution_page
-from .sidebar import sidebar
-
-__all__ = ["dashboard", "character_resolution_page", "sidebar"]
