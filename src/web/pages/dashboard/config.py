9d22036f4fd914e04c95c0337038c02900ff76d5
Arcolaus <arco.work@outlook.com>
2025-09-24 17:48:33 +0800

    start NiceGUI framework

--- src/web/pages/dashboard/config.py
+++
@@ -1,8 +0,0 @@
-_BUTTON_CONFIGS = [
-    {
-        "key": "character_resolution",
-        "icon": "character_resolution",
-        "size": "md",
-        "variant": "secondary",
-    },
-]
--- src/web/pages/dashboard/dashboard.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from src.web.utils.i18n_utils import I18N
-
-
-i18n = I18N("locales", st.session_state.lang)
-i18n.set_scope("dashboard")
-
-st.title(i18n("dashboard"))
-st.subheader(i18n("welcome_message"))
